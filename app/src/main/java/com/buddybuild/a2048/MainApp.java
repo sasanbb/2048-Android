39fa653e5c6d7875b5954f89566e34804f6adafd
Sasan Naderi <sasan@ip-10-254-250-116.us-west-2.compute.internal>
2017-10-11 13:02:19 -0700

    Enable multidex support

--- app/src/main/java/com/buddybuild/a2048/MainApp.java
+++ app/src/main/java/com/buddybuild/a2048/MainApp.java
@@ -1,6 +1,8 @@
 package com.buddybuild.a2048;
 
 import android.app.Application;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 import com.buddybuild.sdk.BuddyBuild;
 
@@ -8,12 +10,12 @@ import com.buddybuild.sdk.BuddyBuild;
  * Created by sasannaderi on 2017-10-05.
  */
 
-public class MainApp extends Application {
+public class MainApp extends MultiDexApplication {
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        MultiDex.install(this);
         BuddyBuild.setup(this);
     }
 }
